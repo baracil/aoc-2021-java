f0e249ab9a31ff331694947ada71c45615501d6c
Bastien Aracil <findbastien@gmail.com>
2021-12-05 16:04:09 +0100

    solve day 4

--- fpc.aoc.problems/fpc.aoc.day04/src/main/java/fpc/aoc/day4/Day4Part1Solver.java
+++ fpc.aoc.problems/fpc.aoc.day04/src/main/java/fpc/aoc/day4/Day4Part1Solver.java
@@ -1,5 +1,8 @@
 package fpc.aoc.day4;
 
+import fpc.aoc.common.AOCException;
+import fpc.aoc.day4.struct.Day04Input;
+import fpc.aoc.day4.struct.GridState;
 import lombok.NonNull;
 import fpc.aoc.api.AOCProblem;
 import fpc.aoc.common.NotSolvedYet;
@@ -9,11 +12,21 @@ import java.util.stream.Stream;
 public class Day4Part1Solver extends Day4Solver {
 
     public static @NonNull AOCProblem<?> provider() {
-        return new Day4Part1Solver().createProblem().skipped();
+        return new Day4Part1Solver().createProblem();
     }
 
     @Override
-    public @NonNull String solve(@NonNull Stream<String> input) {
-        throw new NotSolvedYet();
+    public @NonNull String solve(@NonNull Day04Input input) {
+        Day04Input current = input;
+        do {
+            final var newState = current.playOneRoundPart1().orElse(null);
+            if (newState == null) {
+                throw new AOCException("Cannot solve the problem");
+            }
+            if (newState.gridState() instanceof GridState.Winning winning) {
+                return String.valueOf(winning.score());
+            }
+            current = newState;
+        } while (true);
     }
 }
--- fpc.aoc.problems/fpc.aoc.day04/src/main/java/fpc/aoc/day4/Day4Part2Solver.java
+++ fpc.aoc.problems/fpc.aoc.day04/src/main/java/fpc/aoc/day4/Day4Part2Solver.java
@@ -1,5 +1,8 @@
 package fpc.aoc.day4;
 
+import fpc.aoc.common.AOCException;
+import fpc.aoc.day4.struct.Day04Input;
+import fpc.aoc.day4.struct.GridState;
 import lombok.NonNull;
 import fpc.aoc.api.AOCProblem;
 import fpc.aoc.common.NotSolvedYet;
@@ -9,11 +12,21 @@ import java.util.stream.Stream;
 public class Day4Part2Solver extends Day4Solver {
 
     public static @NonNull AOCProblem<?> provider() {
-        return new Day4Part2Solver().createProblem().skipped();
+        return new Day4Part2Solver().createProblem();
     }
 
     @Override
-    public @NonNull String solve(@NonNull Stream<String> input) {
-        throw new NotSolvedYet();
+    public @NonNull String solve(@NonNull Day04Input input) {
+        Day04Input current = input;
+        do {
+            final var newInput = current.playOneRoundPart2().orElse(null);
+            if (newInput == null) {
+                return switch (current.gridState()) {
+                    case GridState.Winning winning -> String.valueOf(winning.score());
+                    case GridState.NotWinning ignored -> throw new AOCException("Cannot solve the problem");
+                };
+            }
+            current = newInput;
+        } while (true);
     }
 }
